9f0a4ff3e0f9ec2da3c49ca314525c685c52d016
vstreker <vstreker@gmail.com>
2021-02-17 17:24:47 +0600

    ara

--- Third_Lab/Calculator/script.js
+++ Third_Lab/Calculator/script.js
@@ -2,7 +2,7 @@ var vue = new Vue({
     el: '#app',
     data: {
         result: '',
-        numbers: [1,2,4,5,6,7,8,9,0],
+        numbers: [1,2,3,4,5,6,7,8,9,0],
         operations: ['+','-','*','/']
     },
     methods: {
