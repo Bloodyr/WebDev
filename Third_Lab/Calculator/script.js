0607e2a5708e8e847519a9da0546948621982616
vstreker <vstreker@gmail.com>
2021-02-17 17:17:54 +0600

    fa

---
+++ Third_Lab/Calculator/script.js
@@ -0,0 +1,20 @@
+var vue = new Vue({
+    el: '#app',
+    data: {
+        result: '',
+        numbers: [1,2,4,5,6,7,8,9,0],
+        operations: ['+','-','*','/']
+    },
+    methods: {
+        input: function(char) {
+            this.result = this.result.toString();
+            this.result+=char;
+        },
+        reset: function() {
+            this.result = '';
+        },
+        calc: function() {
+            this.result = eval(this.result);
+        }
+    }
+})
